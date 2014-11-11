05ddef53886d661f8c466e00a3fe76c23e2626e5
moznion <moznion@gmail.com>
2014-11-11 21:48:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Regex::Lexer - It's new $module
+
+# SYNOPSIS
+
+    use Regex::Lexer;
+
+# DESCRIPTION
+
+Regex::Lexer is ...
+
+# LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+moznion <moznion@gmail.com>
---
+++ lib/Regex/Lexer.pm
@@ -0,0 +1,39 @@
+package Regex::Lexer;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Regex::Lexer - It's new $module
+
+=head1 SYNOPSIS
+
+    use Regex::Lexer;
+
+=head1 DESCRIPTION
+
+Regex::Lexer is ...
+
+=head1 LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.comE<gt>
+
+=cut
+
